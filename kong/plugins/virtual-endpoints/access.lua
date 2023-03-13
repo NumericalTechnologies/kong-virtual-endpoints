6290a60450d515dde2b51f31009e7db1c40270e1
Prakoso Santoso <prakoso.santoso@numtech.com>
2023-03-13 18:24:56 +0800

    Rewrite virtual endpoint handler

--- kong/plugins/virtual-endpoints/access.lua
+++ kong/plugins/virtual-endpoints/access.lua
@@ -1,77 +1,39 @@
 -- Inspired by https://github.com/Optum/kong-service-virtualization
 local _M = {}
-local resty_sha256 = require "resty.sha256"
-local str = require "resty.string"
-local json = require('cjson')
 local kong = kong
 
-local function sha256aValue(value)
-  local sha256 = resty_sha256:new()
-  sha256:update(value)
-  return str.to_hex(sha256:final())
-end
-
-local function virtualResponse(conf)
-  ngx.status = conf.responseHttpStatus
-  if conf.response and conf.responseContentType then
-    local decodedResponse = ngx.decode_base64(conf.response)
-    ngx.header["Content-Type"] = conf.responseContentType
-    ngx.header["Content-Length"] = #decodedResponse
-    ngx.print(decodedResponse)
-  else
-    ngx.print()
+local function array_contains(array, value_to_find)
+  if type(array) ~= "table" then 
+    kong.log.err("An array is expected." .. " Got: " .. array) 
+    return false
   end
 
-  return ngx.exit(200)
-end
-
-local function virtualNoMatch(expectedSha256, foundSha256)
-  if expectedSha256 and foundSha256 then
-    return kong.response.exit(404, { message = "No virtual request match found, your request yeilded: " .. foundSha256 .. " expected " .. expectedSha256 })
-  else
-    return kong.response.exit(404, { message = "No matching virtual request found!" })
+  for _, value in ipairs(array) do
+    if value == value_to_find then return true end
   end
+
+  return false
 end
 
-function _M.execute(conf)
---Get the List of Virtual Test Cases
-local virtualTests = json.decode(conf.virtual_tests)
- if ngx.req.get_headers()["X-VirtualRequest"] then
-   for i in pairs(virtualTests) do
-      if (ngx.req.get_headers()["X-VirtualRequest"] == virtualTests[i].name and virtualTests[i].requestHttpMethod == ngx.req.get_method()) then
-          if virtualTests[i].requestHash then
-            local foundQueryParameters
-            if ngx.var.request_uri:find('?') then --Is this a URL QUERY based request?
-              foundQueryParameters = ngx.var.request_uri:sub(ngx.var.request_uri:find('?') + 1, ngx.var.request_uri:len())
-            end
+local function respond_virtually(response_config)
+  ngx.header["Content-Type"] = response_config.content_type
+  ngx.print(response_config.data)
+  ngx.exit(response_config.status_code)
+end
 
-            if foundQueryParameters and foundQueryParameters:len() > 2 then -- minimum a=b 3 chars for a URL QUERY based request
-                local sha256foundQueryParameters = sha256aValue(foundQueryParameters)
-                if virtualTests[i].requestHash ~= sha256foundQueryParameters then
-                  virtualNoMatch(virtualTests[i].requestHash, sha256foundQueryParameters)
-                end
-            else
-                ngx.req.read_body()
-                local req_body  = ngx.req.get_body_data()
-                local sha256FoundHttpBody = ""
-                if req_body == nil then
-                  virtualNoMatch(virtualTests[i].requestHash, sha256FoundHttpBody)
-                end
+function _M.execute(config)
+  local endpoints = config.endpoints
+  for index, endpoint in ipairs(endpoints) do
+    local request_config = endpoint.request
+    local response_config = endpoint.response
+    local methods = request_config.methods
 
-                sha256FoundHttpBody = sha256aValue(req_body)
-                if virtualTests[i].requestHash ~= sha256FoundHttpBody then
-                  virtualNoMatch(virtualTests[i].requestHash, sha256FoundHttpBody)
-                end
-            end
-          end
+    if (array_contains(methods, "*") or array_contains(methods, ngx.req.get_method())) then
+      return respond_virtually(response_config)
+    end
+  end
 
-        return virtualResponse(virtualTests[i])
-      end
-   end
-   return virtualNoMatch(nil, nil)
- end
-
- return
+  return kong.response.exit(404, { message = "None of the endpoints matched." })
 end
 
 return _M
