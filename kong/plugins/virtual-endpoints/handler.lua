adb82cf12c8352c272f37e3d6f1990675188ae3a
Prakoso Santoso <prakoso.santoso@numtech.com>
2023-03-15 10:49:57 +0800

    Change plugin priority to 1028

--- kong/plugins/virtual-endpoints/handler.lua
+++ kong/plugins/virtual-endpoints/handler.lua
@@ -2,7 +2,7 @@
 local access = require("kong.plugins.virtual-endpoints.access")
 local KongVirtualEndpointsHandler = {
   VERSION = "1.0.0",
-  PRIORITY = 3000,
+  PRIORITY = 1028,
 }
 
 function KongVirtualEndpointsHandler:access(config)
